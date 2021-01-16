6ce3ea6de9b687d31c4b97bcd57a62098c741a05
tlvandco <lalithvarma12345@gmail.com>
2021-01-16 23:47:05 +0530

    adding all files

---
+++ calci/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def submitquery(request):
+    q = request.GET['query']
+    try:
+        ans = eval(q)
+        mydict = {
+            'q':q,
+            'ans':ans,
+            'error': False
+        }
+        return render(request,'index.html',mydict)
+    except:
+        mydict = {
+            'error': True
+        }
+        return render(request,'index.html',mydict)
